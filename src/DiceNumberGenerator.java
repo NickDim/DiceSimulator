95af096737e3d0ed8eb2131752e27e3fead5f06d
NickDim <nikolad21889@isd273.org>
2017-10-06 21:38:51 -0500

    did stuff

--- src/DiceNumberGenerator.java
+++ src/DiceNumberGenerator.java
@@ -1,45 +1,60 @@
 
 // Dice simulator
-import java.io.FileNotFoundException;
+
+import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.io.File;
-import java.util.Scanner;
 
 public class DiceNumberGenerator {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int numRolls = Integer.parseInt(args[0]);
-		int rollRange = Integer.parseInt(args[1]);
-		
-		PrintWriter numberSaver;
+            int numRolls = Integer.parseInt(args[0]);
+            int rollRange = Integer.parseInt(args[1]);
 
-		try {
-			numberSaver = new PrintWriter("C:\\Users\\nickd\\Desktop\\output.txt");
+            ArrayList<Integer> diceNums = rollADice(numRolls, rollRange);
+            int avg = calculateAverage(diceNums, numRolls);
+            saveToFile(diceNums, avg);
+    }
 
-			ArrayList<Integer> list = new ArrayList<Integer>(numRolls);
+    private static void saveToFile(ArrayList<Integer> diceNums, int avg) {
+        PrintWriter numberSaver;
 
-			for (int count = 0; count < numRolls; count++) {
-				double roll = Math.random() * rollRange;
-				int rollFinal = (int) Math.floor(roll) + 1;
-				list.add(rollFinal);
-				System.out.println(rollFinal);
-			}
-			numberSaver.println(list);
+        try {
+            File output = new File("C:\\Users\\nickd\\Desktop\\output.txt");
+            numberSaver = new PrintWriter(new FileWriter(output, true));
+            numberSaver.println(diceNums);
+            numberSaver.println(diceNums.toString());
+            numberSaver.close();
 
-			int sum = 0;
-			for (int i : list) {
-				sum += list.get(i);
-			}
-			int average = (sum / numRolls);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			System.out.println("The average number was " + average);
-			
-			numberSaver.close();
+    private static int calculateAverage(ArrayList<Integer> list, int numRolls) {
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+        int sum = 0;
+        for (int i : list) {
+            sum += i;
+        }
+        int average = (sum / numRolls);
+
+        System.out.println("The average number was " + average);
+        return average;
+    }
+
+    private static ArrayList<Integer> rollADice(int numRolls, double rollRange) {
+        ArrayList<Integer> Nums = new ArrayList<>();
+        for (int count = 0; count < numRolls; count++) {
+            double roll = Math.random() * rollRange;
+            int rollFinal = (int) Math.floor(roll) + 1;
+            Nums.add(rollFinal);
+            System.out.println(rollFinal);
+        }
+        return Nums;
+
+    }
+
 }
--- src/DiceNumberScanner.java
+++ src/DiceNumberScanner.java
@@ -10,19 +10,31 @@ public class DiceNumberScanner {
 		File output = new File("C:\\Users\\nickd\\Desktop\\output.txt");
 		try {
 
-			ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+			ArrayList<ArrayList<Integer>> lines = new ArrayList<>();
 
 			Scanner scanner = new Scanner(output);
 			// reading output lines from file
+			int count = 1;
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
-				line.substring(1, line.length() - 2);
-				int sum = 0;
-				for (int i = 1; i < list.size(); i++) {
-					sum += list.get(i);
+				line = line.substring(1, line.length() - 2);
+				ArrayList<Integer> intLine = new ArrayList<>();
+				for (String s : line.split(", ")) {
+					intLine.add(Integer.parseInt(s));
 				}
+				lines.add(intLine);
+				int sum = 0;
+				StringBuilder sb = new StringBuilder();
+				for (int i = 0; i < intLine.size(); i++) {
+					
+					sum += intLine.get(i);
+				}
+				sb.append("Run Number" + count);
+				sb.append("Average:   " + sum / intLine.size());
+				System.out.println("Average:   " + sum / intLine.size());
+				count++;
 			}
-			int average = (sum / numberOfRollsFinal);
+			scanner.close();
 
 		}
 
